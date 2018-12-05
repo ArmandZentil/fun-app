0253f55089abc31d6e69a98e113bbaadeb9f6418
Armand Zentil <armandzentil@gmail.com>
2018-12-04 20:09:19 -0500

    changed html in app js file

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            My dawg is the light of my life
           </a>
         </header>
       </div>
